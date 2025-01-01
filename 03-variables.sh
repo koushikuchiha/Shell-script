a79996732bf19c96026e2d8942bb7cab27251db0
koushik changala <you@example.com>
2025-01-01 23:59:30 +0530

    using variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Aparna
+PERSON2=Gnavi
+
+echo "$PERSON1: : Hi $PERSON2"
+echo "$PERSON2: : Hello $PERSON1"
+echo "$PERSON1: : How are doing"
+echo "$PERSON2: : I am doing good"
