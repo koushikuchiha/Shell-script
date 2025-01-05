1391c132a6530023c0335cda297475f15bcf1871
koushik changala <you@example.com>
2025-01-05 17:52:49 +0530

    shellscript

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -5,5 +5,5 @@ MOVIES=("KGF" "RRR" "SALAR")
 echo "First Movie is: ${MOVIES[0]}"
 echo "Second Movie is: ${MOVIES[1]}"
 echo "Third Movie is: ${MOVIES[2]}"
-echo "All Movies list: $MOVIES"
+echo "All Movies list: ${MOVIES[@]}"
 
