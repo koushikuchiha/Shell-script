e1e0456d2d37f1e0782c72509b629d1c448ca36e
koushik changala <you@example.com>
2025-01-05 17:30:26 +0530

    shellscript

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
-NUMBER=$1
-NUMBER=$2
-
+NUMBER1=$1
+NUMBER2=$2
 TIMESTAMP=$(date)
-
 echo "script executed at: $TIMESTAMP"
-
-SUM=$(($NUMBER1+$NUMBER2))
-
-echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
+SUM=$(($NUMBER1 + $NUMBER2))
+echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM
 
 
