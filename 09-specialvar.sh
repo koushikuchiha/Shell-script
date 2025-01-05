0c3ef0ab536eb14959d94f0ab611f69126ec41e0
koushik changala <you@example.com>
2025-01-05 23:45:40 +0530

    shell script

--- 09-specialvar.sh
+++ 09-specialvar.sh
@@ -7,4 +7,5 @@ echo "Present Working Directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running the script: $USER"
 echo "Process id of current script: $$"
+sleep 60 &
 echo "Process id of last command in background: $!"
