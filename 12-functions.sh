f1e520be6b3751fcd6f316f30375994edec14bad
koushik changala <you@example.com>
2025-01-14 00:27:05 +0530

    shell script

---
+++ 12-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$( id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo " Installing Mysql.... Failure"
+        exit1
+    else
+        echo "installing Mysql......Success"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Error: you must have sudo access to execute the script"
+    exit 1
+fi
+dnf list installed mysql -y
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    VALIDATE $?
+else 
+    echo "Mysql is already installed"
+fi
