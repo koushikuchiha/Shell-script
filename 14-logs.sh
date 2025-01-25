92a3624d8a2e217f1838a47adf20c7832d4fa774
koushik changala <you@example.com>
2025-01-25 14:03:28 +0530

    shellscript

--- 14-logs.sh
+++ 14-logs.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[34m"
 
-LOGS_FOLDER=$("/var/log/shellscripts-logs")
+LOGS_FOLDER="/var/log/shellscripts-logs"
 LOG_FILE=$( echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H:%M:%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
